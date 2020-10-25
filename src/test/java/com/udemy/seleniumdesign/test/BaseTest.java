677efd55d84f6ad3b391ec927028d21c2a7eb57c
Vakerin Eugene <utyfdfrthby08@gmail.com>
2020-10-25 18:30:21 +0200

    last

--- src/test/java/com/udemy/seleniumdesign/test/BaseTest.java
+++ src/test/java/com/udemy/seleniumdesign/test/BaseTest.java
@@ -3,6 +3,7 @@ package com.udemy.seleniumdesign.test;
 import com.google.common.util.concurrent.Uninterruptibles;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -15,10 +16,8 @@ public class BaseTest {
 
     @BeforeTest
     public void setupDriver() {
-        //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        //System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
-        WebDriverManager.firefoxdriver().setup();
-        this.driver = new FirefoxDriver();
+        WebDriverManager.chromedriver().setup();
+        this.driver = new ChromeDriver();
     }
 
     @AfterTest
